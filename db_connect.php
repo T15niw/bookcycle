a12819e8c872fb216fae517d036426a29bf2a779
T15niw <tasnim8mezgueldi@gmail.com>
2025-06-24 12:36:52 +0100

    d

--- db_connect.php
+++ db_connect.php
@@ -5,7 +5,7 @@ $user = 'root';
 $password = ''; 
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: ");
